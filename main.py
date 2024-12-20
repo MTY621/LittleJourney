b4ccb811326d6520ec7368261327d678a9ff52bd
MTY621 <mty210205@gmail.com>
2024-12-20 17:05:44 +0200

    baba

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ def print_hi(name):
     #bunicul gras
     #bunicul slab
 
+    #baba
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
