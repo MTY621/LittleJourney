c64facfb689d8dbfefe728b177c570fe6e1b38f6
MTY621 <mty210205@gmail.com>
2024-12-20 17:02:13 +0200

    radul

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ def print_hi(name):
     #bunicul gras
     #bunicul slab
 
+    #radu
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
