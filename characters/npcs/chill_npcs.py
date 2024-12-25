fa8bf5eefeb1404ccfa4b9e96bc0755435b9b799
MTY621 <mty210205@gmail.com>
2024-12-25 15:32:55 +0200

    added character instances

--- characters/npcs/chill_npcs.py
+++ characters/npcs/chill_npcs.py
@@ -1,5 +1,19 @@
 from characters.chill_npc import ChillNpc
 
 chill_npcs = []
-chill_npcs.append(ChillNpc("main_character/Orc", 10, 20, 2, 5, 0,
-                   10, "orc", [], 0))
+
+#0
+chill_npcs.append(ChillNpc("peasant", 10, 10, 0, 0, 0,
+                   0, "Poor peasant", [], 0))
+#0
+chill_npcs.append(ChillNpc("marry", 8, 8, 0, 0, 0,
+                   1, "Marry", [], 0))
+#0
+chill_npcs.append(ChillNpc("big_betty", 1000, 1000, 100, 100, 0,
+                   0, "BIG BETTY", [], 0))#cap de porc 100 sansa
+#0
+chill_npcs.append(ChillNpc("muslim_man", 25, 25, 3, 5, 10,
+                   12, "Ahmet", [], 0))
+#0
+chill_npcs.append(ChillNpc("muslim_woman", 20, 20, 2, 4, 0,
+                   0, "Aaliya", [], 0))
