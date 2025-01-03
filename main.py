4ecb8aa2c7e13fcff47edf7e68ec2aa6b29a2609
BiganRadu <radubigan5@gmail.com>
2025-01-03 04:45:41 +0200

    solve some proiblem

--- main.py
+++ main.py
@@ -1,22 +1,48 @@
-# This is a sample Python script.
+from time import sleep
+import pygame
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import glob
+from menus.main_menu import menu
+from menus.story_menus.gameplay_menus import menus
+from characters.npcs.chill_npcs import chill_npcs
+from characters.npcs.fighting_npcs import fighting_npcs
+from game.game import Game
 
+# pygame.init()
+# pygame.mixer.init()
+# extra_info.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+# extra_info.display = pygame.display
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-    # mama mare
-    # tata mic si mare si tare
-
-    #bunicul gras
-    #bunicul slab
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    menu()
+    game = Game(glob.player_race, glob.player_name, menus[1], glob.MAIN_MENU_SONG)
+    for chill_npc in chill_npcs:
+        chill_npc.game = game
+    for fighting_npc in fighting_npcs:
+        fighting_npc.game = game
+    for menu in menus:
+        menu.game = game
+    # extra_info.background = pygame.image.load('background/castle/1_garden.png').convert()
+    # extra_info.background = pygame.transform.scale(extra_info.background, (SCREEN_WIDTH, SCREEN_HEIGHT))
+    game.start()
+    # i = 4
+    # clock = pygame.time.Clock()
+    # extra_info.player = Player("Human", 100, "Player", [])
+    # cur_menu = menu_1
+    # while True:
+    #     extra_info.screen.blit(extra_info.background, (0, 0))
+    #     cur_menu = gameplay_menu(cur_menu)
+    #     if cur_menu is None:
+    #         break
+    #     # i -= 1
+    #     #
+    #     # player.attack(screen, pygame.display, background)
+    #     # sleep(0.5)
+    #     # player.take_damage(screen, 25, pygame.display, background)
+    #     # sleep(0.5)
+    #
+    #     clock.tick(60)
+    #
+    # sleep(2)
+    # pygame.mixer.quit()
+    # pygame.quit()
